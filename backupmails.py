bb4238ed0a3e722bb2abcea22401f417d740c701
Maxime Mérian <maxime@merian.me>
2016-11-15 14:46:32 +0100

    Multiple enhancements
    - Added --timeout and --continue options
    - Use logging facility for displaying messages
    - Fix slash in imap folder name
    - Handle signals

--- backupmails.py
+++ backupmails.py
@@ -12,8 +12,11 @@ import os
 import argparse
 import sys
 import email
+import logging
 import mailbox
 import re
+import signal
+import socket
 
 from getpass import getpass
 from imaplib import IMAP4,IMAP4_SSL
@@ -21,47 +24,84 @@ from imaplib import IMAP4,IMAP4_SSL
 parser = argparse.ArgumentParser(description = 'Creates mailbox files from messages on an IMAP server')
 
 parser.add_argument('--host', help = 'mail server host name', required = True)
-parser.add_argument('--port', help = 'mail server port', type=int, default = 143)
+parser.add_argument('--port', help = 'mail server port', type = int, default = 143)
 parser.add_argument('--username', help = 'mail server username', required = True)
 parser.add_argument('--password', help = 'mail server password', required = False, default = None)
 parser.add_argument('--mboxprefix', help = 'mailbox filename prefix', required = False, default = '')
-parser.add_argument('--ssl', help = 'use SSL connection', action='store_true')
+parser.add_argument('--ssl', help = 'use SSL connection', action = 'store_true')
 parser.add_argument('--dest-dir', help = 'Mailbox files destination directory', required = False, default = None)
+parser.add_argument('--timeout', help = 'connection timeout', type = float, default = None)
+parser.add_argument('--continue', help = 'keep existing mailbox files and append new messages', action = 'store_true', dest = 'cont')
 
 args = parser.parse_args()
 
 client = None
 mbox = None
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 list_response_pattern = re.compile(r'\((?P<flags>.*?)\) "(?P<delimiter>.*)" (?P<name>.*)')
 
 def backup_imap_folder(folder):
-    print folder
+    global mbox
+    global logger
+    logger.info('Processing folder '+folder)
     try:
         client.select(folder, True)
         r, data = client.uid('search', None, 'ALL')
-        mboxfilename = args.mboxprefix+folder+'.mbox'
+        mboxfilename = args.mboxprefix+folder.replace('"', '').replace('/', '.')+'.mbox'
         if args.dest_dir is not None:
             mboxfilename = os.path.join(args.dest_dir, mboxfilename)
         mbox = mailbox.mbox(mboxfilename)
         mbox.lock()
-        mbox.clear()
-        for msgid in data[0].split():
+        if not args.cont:
+            mbox.clear()
+            mbox.flush()
+        msgids = data[0].split()
+        nummsgs = len(msgids)
+        if 1 == nummsgs:
+            logger.info('1 message in folder')
+        else:
+            logger.info(str(nummsgs)+' messages in folder')
+        processed = 0
+        for msgid in msgids:
             try:
                 r, msgdata = client.uid('fetch', msgid, '(RFC822)')
                 message = email.message_from_string(msgdata[0][1])
+                if args.cont and message.has_key('Message-ID'):
+                    for m in mbox:
+                        if (m.has_key('Message-ID')) and (message['Message-ID'] == m['Message-ID']):
+                            logger.info('Message '+message['Message-ID']+' already in mailbox')
+                            continue
                 mbox.add(message)
             except Exception as e:
-                print e
-                pass
+                logger.exception(e)
+            processed += 1
+            if 0 == processed % 50:
+                logger.info(str(processed)+'/'+str(nummsgs)+' messages processed')
         mbox.flush()
+        mbox.unlock()
         mbox.close()
     except Exception as e:
-        print e
-        pass
+        logger.exception(e)
+
+def handle_signal(signal, frame):
+    global mbox
+    global logger
+    logger.info('Received signal. Exiting')
+    mbox.flush()
+    mbox.unlock()
+    mbox.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_signal)
 
 # Entry point
 try:
+    # Set socket timeout
+    if args.timeout is not None:
+        socket.setdefaulttimeout(args.timeout)
     # Check for password
     password = args.password
     if password is None:
@@ -73,11 +113,14 @@ try:
         ssl = True
 
     # Login to IMAP server
+    logger.info('Connecting to mail server')
     if ssl:
         client = IMAP4_SSL(args.host, args.port)
     else:
         client = IMAP4(args.host, args.port)
+    logger.info('Logging in')
     client.login(args.username, password)
+    logger.info('Logged in')
 
     # Create dest dir if not exists
     if args.dest_dir is not None:
@@ -85,7 +128,7 @@ try:
             os.mkdir(args.dest_dir)
 
 except Exception as e:
-    print(e)
+    logger.exception(e)
     sys.exit(2)
 
 # Iterate through all IMAP folders and backup messages
@@ -96,5 +139,5 @@ if 'OK' == r:
         try:
             backup_imap_folder(mailbox_name)
         except Exception as e:
-            print e
+            logger.exception(e)
             pass
